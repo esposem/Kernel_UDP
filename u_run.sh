325240e173585beb2cf0096badc91ad8266d29ee
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-02-03 21:49:14 +0100

    fixed script

--- u_run.sh
+++ u_run.sh
@@ -3,7 +3,7 @@ make > /dev/null
 
 # ${2:-"127.0.0.1"} means that second arg ($2) get set to "127.0.0.1" if not set
 if [[ $1 == "c" ]]; then
-  ./user_udp/user_client ${2:-"127.0.0.1"} ${3:-"3000"}
+  ./user_udp/user_client ${2:-"127.0.0.1"} ${3:-"4000"} ${4:-"127.0.0.1"} ${5:-"3000"}
 fi
 
 if [[ $1 == "s" ]]; then
