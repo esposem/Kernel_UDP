108f991582042eb668506e1dff42b11955e760e8
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-05-14 23:07:17 +0200

    handled change of header for signal

--- kern_udp/include/kernel_message.h
+++ kern_udp/include/kernel_message.h
@@ -1,7 +1,14 @@
 #ifndef K_MESS
 #define K_MESS
 
+#include <linux/version.h>
 #include <net/sock.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+#include <linux/sched/signal.h>
+#else
+#include <linux/signal.h>
+#endif
 
 typedef struct message_data message_data;
 
@@ -13,22 +20,24 @@ typedef struct message_data message_data;
 #define TEST "TESTL"
 
 extern int MAX_MESS_SIZE;
-extern message_data * request;
-extern message_data *  reply;
+extern message_data *request;
+extern message_data *reply;
 
 extern void init_default_messages(void);
 extern void del_default_messages(void);
 
-extern void fill_sockaddr_in(struct sockaddr_in * addr, unsigned char *  ip, int flag, int port);
-extern void division(size_t dividend, size_t divisor, char * result, size_t size_res);
+extern void fill_sockaddr_in(struct sockaddr_in *addr, unsigned char *ip,
+                             int flag, int port);
+extern void division(size_t dividend, size_t divisor, char *result,
+                     size_t size_res);
 
-extern message_data * create_rcv_message(void);
-extern message_data * create_message(char * data, size_t size_data, int id);
-extern char * get_message_data(message_data * mess);
-extern size_t get_message_size(message_data * mess);
-extern int get_message_id(message_data * mess);
-extern void set_message_id(message_data * mess, int id);
-extern size_t get_total_mess_size(message_data * mess);
-extern void delete_message(message_data * mess);
+extern message_data *create_rcv_message(void);
+extern message_data *create_message(char *data, size_t size_data, int id);
+extern char *get_message_data(message_data *mess);
+extern size_t get_message_size(message_data *mess);
+extern int get_message_id(message_data *mess);
+extern void set_message_id(message_data *mess, int id);
+extern size_t get_total_mess_size(message_data *mess);
+extern void delete_message(message_data *mess);
 
 #endif
