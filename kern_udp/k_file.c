0acffd9bb57705c45929bc630ab66b5156cb54bf
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-05-14 23:18:58 +0200

    deleted uaccess.h

--- kern_udp/k_file.c
+++ kern_udp/k_file.c
@@ -1,17 +1,14 @@
 #include "k_file.h"
 #include <asm/segment.h>
-#include <asm/uaccess.h>
 #include <linux/buffer_head.h>
 
 // Taken from
 // https://stackoverflow.com/questions/1184274/how-to-read-write-files-within-a-linux-kernel-module
 
-struct file*
-file_open(const char* path, int flags, int rights)
-{
-  struct file* filp = NULL;
+struct file *file_open(const char *path, int flags, int rights) {
+  struct file *filp = NULL;
   mm_segment_t oldfs;
-  int          err = 0;
+  int err = 0;
 
   oldfs = get_fs();
   set_fs(get_ds());
@@ -25,19 +22,15 @@ file_open(const char* path, int flags, int rights)
   return filp;
 }
 
-void
-file_close(struct file* file)
-{
+void file_close(struct file *file) {
   if (file)
     filp_close(file, NULL);
 }
 
-int
-file_read(struct file* file, unsigned long long offset, unsigned char* data,
-          unsigned int size)
-{
+int file_read(struct file *file, unsigned long long offset, unsigned char *data,
+              unsigned int size) {
   mm_segment_t oldfs;
-  int          ret;
+  int ret;
 
   oldfs = get_fs();
   set_fs(get_ds());
@@ -46,12 +39,10 @@ file_read(struct file* file, unsigned long long offset, unsigned char* data,
   return ret;
 }
 
-int
-file_write(struct file* file, unsigned long long offset, unsigned char* data,
-           unsigned int size)
-{
+int file_write(struct file *file, unsigned long long offset,
+               unsigned char *data, unsigned int size) {
   mm_segment_t oldfs;
-  int          ret;
+  int ret;
 
   oldfs = get_fs();
   set_fs(get_ds());
@@ -60,9 +51,7 @@ file_write(struct file* file, unsigned long long offset, unsigned char* data,
   return ret;
 }
 
-int
-file_sync(struct file* file)
-{
+int file_sync(struct file *file) {
   vfs_fsync(file, 0);
   return 0;
 }
